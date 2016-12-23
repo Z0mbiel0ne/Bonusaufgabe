9e728ccc50d1b6cc198b3c751c1316b2d99c945b
stkli002 <stephan.klimek002@stud.fh-dortmund.de>
2016-12-23 21:38:46 +0100

    SQL-Services & ConnectionManagment
    #new class not used
    #no fixes
    + ConnectionManager class
    + SQLServices class

--- src/fh/bonus/SQLService.java
+++ src/fh/bonus/SQLService.java
@@ -2,20 +2,33 @@ package fh.bonus;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- *
+ * SQL Service
+ * 
  * @author ExaShox
  */
 public class SQLService {
-    private Connection con = null;
-    private Statement stmt = null;
-    private ResultSet rs = null;
-
-    public SQLService () {
-        con  = ConnectionManager.getConnection();
-        stmt  = con.createStatement();
+    private Connection conn;
+    private Statement stmt;
+    private ResultSet rs;
+    
+    /**
+     * Get data from Database
+     * 
+     * @param sql
+     * @return ResultSet
+     * @throws SQLException 
+     */
+    public ResultSet get(String sql) throws SQLException {
+        conn  = ConnectionManager.getConnection();
+        stmt  = conn.createStatement();
         rs  = stmt.executeQuery(sql);
+        
+        conn.close();
+        stmt.close();
+        return rs;
     }
 }
